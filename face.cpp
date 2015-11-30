3681de73494dc2f6b4fd7f7969831a482301cfe3
funnyBrain <hsuhsiaohsuan+play@gmail.com>
2015-12-01 07:44:53 +0800

    [2015/12/01]     1. temp.

---
+++ face.cpp
@@ -0,0 +1,18 @@
+#include "opencv2/core/core.hpp"
+//#include "opencv2/contrib/contrib.hpp"
+#include "opencv2/face.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, const char* argv[]) {
+
+    return 0;
+}
